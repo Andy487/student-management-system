c3d075d8e71385e1b3a7e182501eee9648f71745
Anand Nayak <anandnayak95@gmail.com>
2021-02-14 11:11:56 +0530

    first commit

---
+++ sms/src/main/java/org/school/repository/CustomerRepository.java
@@ -0,0 +1,26 @@
+package org.school.repository;
+
+import org.school.model.Student;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Profile("mysql")
+@ComponentScan
+@Repository
+public interface CustomerRepository extends CrudRepository<Student, Long> {
+
+    List<Student>findByFirstName(String firstName);
+
+   // List<Student> findByAddress(String address);
+
+    Student findById(long id);
+
+   long count();
+
+    @Override
+    void deleteAll();
+}
